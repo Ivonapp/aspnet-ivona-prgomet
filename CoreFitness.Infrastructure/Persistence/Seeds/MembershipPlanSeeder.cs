09514bcfed4a299b49d5617dfa4148c52bedd7cd
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-03-20 10:23:30 +0100

    building the seed section with seedasync and not modelbuilder.

---
+++ CoreFitness.Infrastructure/Persistence/Seeds/MembershipPlanSeeder.cs
@@ -0,0 +1,10 @@
+namespace CoreFitness.Infrastructure.Persistence.Seeds;
+
+public static class MembershipPlanSeeder
+{
+    public static async Task SeedAsync(ApplicationDbContext context)
+    {
+
+
+    }
+}
