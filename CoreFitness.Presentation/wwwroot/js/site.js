a9144074239ca76d00425ebd5151218d319f4390
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-03-24 14:25:28 +0100

    Still working on the hamburger menu in navbar.

--- CoreFitness.Presentation/wwwroot/js/site.js
+++ CoreFitness.Presentation/wwwroot/js/site.js
@@ -3,4 +3,10 @@
 darkmode, lightmode, on toggle, */
 
 
+const hamburger = document.querySelector(".dropdown")
+const navMenu = document.querySelector(".hamburger-links")
 
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
