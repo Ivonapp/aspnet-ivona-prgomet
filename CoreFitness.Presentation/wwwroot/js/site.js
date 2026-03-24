f1cdec47df3bfbff2a53acd15e7dd23732137748
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-03-24 20:33:46 +0100

    Almost finished with the accordion.

--- CoreFitness.Presentation/wwwroot/js/site.js
+++ CoreFitness.Presentation/wwwroot/js/site.js
@@ -2,6 +2,14 @@
 
 darkmode, lightmode, on toggle, */
 
+/* 
+    Tog lite hjälp av W3 schools för javascript delen
+    samt kollade jag på lite olika videos för att förstå,
+    där jag tyckte denna videos:
+    https://youtu.be/flItyHiDm7E?si=HzozqQfUniHHfXsY
+    var bra då jag kunde hänga med i det han gjorde
+    och förstod. 
+*/
 
 const hamburger = document.querySelector(".dropdown")
 const navMenu = document.querySelector(".hamburger-menu")
@@ -12,11 +20,16 @@ hamburger.addEventListener("click", () => {
 })
 
 
-/* 
-    Tog lite hjälp av W3 schools för javascript delen
-    samt kollade jag på lite olika videos för att förstå,
-    där jag tyckte denna videos:
-    https://youtu.be/flItyHiDm7E?si=HzozqQfUniHHfXsY
-    var bra då jag kunde hänga med i det han gjorde
-    och förstod. 
-*/
+
+
+
+
+// ACCORDION icon, pilen pekar upp och ner
+    const accordionArrow = document.querySelectorAll(".accordion-question");
+
+    accordionArrow.forEach(question => {
+        question.addEventListener("click", () => {
+            const item = question.parentElement;
+            item.classList.toggle("active");
+    });
+});
