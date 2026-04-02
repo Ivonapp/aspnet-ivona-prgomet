d4bad33ba9999070284d7ae270e5eb594921202c
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-04-02 14:48:46 +0200

    Slowly building up the fileupload for the Account section. I first created a separate Models and Controller for the FileUpload, however, due to the FileUpload not being on a separate page, I have instead added it into the Controller and the Model for the MyAccount. This due to making the code more clean AND due to the fact I can only add one @model i the MyAccount.cshtml. If I stumble across any other issues, I'll deal with them then. This still follows the clean architecture, and due to the customer adding in the information as a img or file it's also technically a formModel and not a viewModel.

--- CoreFitness.Presentation/Controllers/MyAccountController.cs
+++ CoreFitness.Presentation/Controllers/MyAccountController.cs
@@ -4,9 +4,22 @@ namespace CoreFitness.Presentation.Controllers;
 
 public class MyAccountController : Controller
 {
+
+//MYACCOUNT SIDAN
     public IActionResult MyAccount()
     {
         return View("~/Views/Account/MyAccount.cshtml");
     }
 
+
+
+//FILUPPLADDNING
+    [HttpPost]
+    public IActionResult Upload()
+    {
+        // Logik för uppladdning kommer här sen
+        return RedirectToAction("MyAccount");
+    }
 }
+
+
--- CoreFitness.Presentation/Models/MyAccountFormModel.cs
+++ CoreFitness.Presentation/Models/MyAccountFormModel.cs
@@ -1,27 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations;
-// namespace CoreFitness.Presentation.Models;
+namespace CoreFitness.Presentation.Models;
 
     public class MyAccountFormModel
 {
     
-[Display(Name ="FirstName", Prompt = "Enter First Name")]                //Det som står i Placeholdern
-public string FirstName { get; set; }
+    [Display(Name ="FirstName", Prompt = "Enter First Name")]                //Det som står i Placeholdern
+    public string FirstName { get; set; } = null!;
 
 
-[Display(Name ="LastName", Prompt = "Enter Last Name")]                //Det som står i Placeholdern
-public string LastName { get; set;}
+    [Display(Name ="LastName", Prompt = "Enter Last Name")]                //Det som står i Placeholdern
+    public string LastName { get; set; } = null!;
 
 
-[Display(Name ="Email", Prompt = "username@domain.com")]                //Det som står i Placeholdern
-[DataType(DataType.EmailAddress)]                                       // DataTypen är email
-[Required(ErrorMessage = "Email Address is required.")]                 //Error meddelande som skrivs ut
-[RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "You must enter a valid email address.")] // REGEX för email
-public string Email { get; set;}
+    [Display(Name = "Email", Prompt = "username@domain.com")]                //Det som står i Placeholdern
+    [DataType(DataType.EmailAddress)]                                       // DataTypen är email
+    [Required(ErrorMessage = "Email Address is required.")]                 //Error meddelande som skrivs ut
+    [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "You must enter a valid email address.")] // REGEX för email
+    public string Email { get; set; } = null!;
 
 
-[Display(Name ="PhoneNumber", Prompt = "Enter Phone Number")]                //Det som står i Placeholdern
-public string PhoneNumber { get; set; }
+    [Display(Name ="PhoneNumber", Prompt = "Enter Phone Number")]                //Det som står i Placeholdern
+    public string? PhoneNumber { get; set; }
 
+
+
+
+
+
+    // NEDAN FÖR BILDFIL SOM KUND SKA LÄGGA TILL
+    [Required(ErrorMessage = "Please select a file to upload.")]
+    public IFormFile File { get; set; } = null!;
 
 }
 
