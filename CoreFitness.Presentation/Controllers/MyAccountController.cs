9feefc5159eba119371aa2a5f1d295ce7ec9edd5
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-04-23 15:16:02 +0200

    Currently working on building up the Service regarding saving the users profile information.

--- CoreFitness.Presentation/Controllers/MyAccountController.cs
+++ CoreFitness.Presentation/Controllers/MyAccountController.cs
@@ -22,8 +22,9 @@ public class MyAccountController(IWebHostEnvironment env, AccountService account
 
 
 
-    //MYACCOUNT SIDAN
+    //MYACCOUNT SIDAN / SPARA SIDA
     [HttpGet]
+    [Route("myaccount")]
     public IActionResult MyAccount()
     {
         return View("~/Views/Account/MyAccount.cshtml");
@@ -31,36 +32,44 @@ public class MyAccountController(IWebHostEnvironment env, AccountService account
 
 
 
-    //FILUPPLADDNING
-    [HttpGet]
-    public IActionResult Upload()
+
+
+
+    [HttpPost]
+    [Route("myaccount")]
+
+    public IActionResult MyAccount(MyAccountFormModel model)
     {
         
-        return View();
-    }
 
+        // 1. ID: Identifiera vilken användare som är inloggad.
+         var findUser = _userManager.GetUserId(User);    
 
-
-
-                //FILUPPLADDNING
-    [HttpPost]
-    public async Task<IActionResult> Upload(MyAccountFormModel formData) //Inuti Paranteserna skriver vi FormModel som styr filuppladdning. I detta fallet finns den i MyAccountFormModel.
-    {
-        if (!ModelState.IsValid || formData.File == null || formData.File.Length == 0)
-            return View("~/Views/Account/MyAccount.cshtml", formData);
-
-        var uploadFolder = Path.Combine(_env.WebRootPath, "Uploads");               //folder för bild
-        Directory.CreateDirectory(uploadFolder);                                    //Om foldern inte finns så skapas en
-
-        var filePath = Path.Combine(uploadFolder, $"{Guid.NewGuid()}_{Path.GetFileName(formData.File.FileName)}"); // bILDen användaren laddar upp sparas här, samt får den ett unikt GUID så man kan lägga in samma bild fler gånger där den får ett unikt namn
-
-        using (var stream = new FileStream(filePath, FileMode.Create))
+        // 2. Säkerhetskontroll: Avbryt och skicka bort besökaren om ingen användare hittas.
+        if (findUser == null)
         {
-            await formData.File.CopyToAsync(stream);   
+            TempData["Error"] = "";
+            return RedirectToAction("Index", "Home");
         }
 
-         ViewBag.Message = "File was uploaded successfully.";
-        return View("~/Views/Account/MyAccount.cshtml", formData);
+        //3. Validering: Kontrollera att det som skrivits i formuläret stämmer överens med dina krav i formModel.
+        if(!ModelState.IsValid)
+        {
+            return View("~/Views/Account/DeleteAccount.cshtml", model);
+        }
+
+
+
+        //> Om fel: Stanna kvar på sidan och visa vyn igen med befintlig data.
+
+        //5. Spara den nya informationen i databasen genom att anropa Service.
+
+
+
+        //6. Resultathantering:
+        //> Vid lyckat resultat: Informera användaren om att det är sparat och ladda om eller visa sidan på nytt.
+        //> Vid misslyckat resultat: Skapa ett felmeddelande och stanna kvar på sidan för att låta användaren försöka igen.
+
     }
 
 
@@ -70,7 +79,22 @@ public class MyAccountController(IWebHostEnvironment env, AccountService account
 
 
 
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* RADERA KONTO */
     [HttpGet]
     [Route("removeaccount")]
     public async Task<IActionResult> DeleteAccount()
@@ -86,30 +110,31 @@ public class MyAccountController(IWebHostEnvironment env, AccountService account
     public async Task<IActionResult> DeleteAccount(DeleteAccountFormModel model)
     {
 
-        // Hämta inloggad användares ID.
+        // 1. ID: Identifiera vilken användare som är inloggad.
         var findUser = _userManager.GetUserId(User);    // _userManager kollar bara i User. User är en inbyggd egenskap i Controller. Den innehåller all information om personen som surfar på sidan just nu – men informationen ligger i en så kallad Cookie i webbläsaren. Denna information kallas för "Claims".
                                                         // Ingen await = Eftersom vi endast läser webbläsarens cookie, behöver programmet inte vänta på svar från databasen för att hämta det.
 
+        // 2. Säkerhetskontroll: Avbryt och skicka bort besökaren om ingen användare hittas.
         if (findUser == null)                       // OM användaren inte hittas, laddas sidan om.
         {
             TempData["Error"] = "";
             return RedirectToAction("Index", "Home");
         }
 
-        // 1A CHECK - Har användaren fyllt i formuläret korrekt? (FormModel)
-        if (!ModelState.IsValid)
+        //3. Validering: Kontrollera att det som skrivits i formuläret stämmer överens med dina krav i formModel.
+        if (!ModelState.IsValid)                // > 1A CHECK - Har användaren fyllt i formuläret korrekt? (FormModel)
         {
             return View("~/Views/Account/DeleteAccount.cshtml", model);
         }
         
-
-        // 2A CHECK - ÄR CHECKBOX IFYLLD?*
-        if(!model.ConfirmDelete)
+       
+        if(!model.ConfirmDelete)                // > 2A CHECK - ÄR CHECKBOX IFYLLD?*
         {
             ModelState.AddModelError("ConfirmDelete", "Please confirm that you have read the terms and conditions.");
             return View("~/Views/Account/DeleteAccount.cshtml", model);
         }
 
+        // ***************************************************************************
 
         // Kör din service-metod för RADERING av kontot i databasen.
 
@@ -129,26 +154,35 @@ public class MyAccountController(IWebHostEnvironment env, AccountService account
 
 
 
+        //FILUPPLADDNING
+        [HttpGet]
+        public IActionResult Upload()
+        {
+
+            return View();
+        }
 
 
+        //FILUPPLADDNING
+        [HttpPost]
+        public async Task<IActionResult> Upload(MyAccountFormModel formData) //Inuti Paranteserna skriver vi FormModel som styr filuppladdning. I detta fallet finns den i MyAccountFormModel.
+        {
+            if (!ModelState.IsValid || formData.File == null || formData.File.Length == 0)
+                return View("~/Views/Account/MyAccount.cshtml", formData);
 
+            var uploadFolder = Path.Combine(_env.WebRootPath, "Uploads");               //folder för bild
+            Directory.CreateDirectory(uploadFolder);                                    //Om foldern inte finns så skapas en
 
+            var filePath = Path.Combine(uploadFolder, $"{Guid.NewGuid()}_{Path.GetFileName(formData.File.FileName)}"); // bILDen användaren laddar upp sparas här, samt får den ett unikt GUID så man kan lägga in samma bild fler gånger där den får ett unikt namn
 
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await formData.File.CopyToAsync(stream);
+            }
 
-
-
-
-
-    // ***SKRIV KODEN FÖR ATT SPARA ANVÄNDARENS UPPGIFTER - SERVICE FÄRDIG***
-
-
-
-
-
-
-
-
-
+            ViewBag.Message = "File was uploaded successfully.";
+            return View("~/Views/Account/MyAccount.cshtml", formData);
+     }
 }
 
 
